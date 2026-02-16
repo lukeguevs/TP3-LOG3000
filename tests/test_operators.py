933806365ee97c14784518a8121daa0cab77c6f0
Lucas Guevremont <144279949+lukeguevs@users.noreply.github.com>
2026-02-15 20:40:58 -0500

    feat: ajout de tests pour la division avec nombres à virgule

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -54,6 +54,8 @@ class TestOperators(unittest.TestCase):
         self.assertEqual(divide(6, 3), 2)
         # Vérification de la division entière (troncature)
         self.assertEqual(divide(5, 2), 2)
+        # Vérification de la division avec des nombres flottants
+        self.assertEqual(divide(10, 4), 2.5)
         # Vérifie qu'une division par zéro lève une exception
         with self.assertRaises(ZeroDivisionError):
             divide(1, 0)
